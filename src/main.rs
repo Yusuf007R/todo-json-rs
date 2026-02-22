38b73d1bb070769a33a7def10bca89a91fc9ca67
Yusuf007R <yusufmaster7@gmail.com>
2026-02-22 16:33:20 -0500

    feat(todo): finish v1

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use serde::{Deserialize, Serialize};
 use std::fs::{self};
 use time::OffsetDateTime;
 
+use std::io::{self, Write};
+
 #[derive(Subcommand, Debug)]
 enum Commands {
     /// The `Add` command takes a `content` string argument
-    Add { content: String },
+    Add { content: Vec<String> },
     /// The `Rm` command takes an `id` number argument.
     Rm { id: u32 },
     /// The `Ls` command lists all todos.
@@ -14,7 +16,7 @@ enum Commands {
     /// The `Done` command takes an `id` number argument.
     Done { id: u32 },
     /// The `Edit` command takes an `id` number argument and a `content` string argument.
-    Edit { id: u32, content: String },
+    Edit { id: u32, content: Vec<String> },
 }
 
 #[derive(Parser, Debug)]
@@ -65,7 +67,7 @@ fn main() {
         Commands::Add { content } => {
             let todo = Todo {
                 id: db.next_id,
-                content,
+                content: content.join(" "),
                 completed_at: None,
                 created_at: OffsetDateTime::now_utc(),
                 updated_at: OffsetDateTime::now_utc(),
@@ -75,12 +77,60 @@ fn main() {
             save_db(db);
         }
 
-        Commands::List => {
-            println!("{:?}", db.todos)
+        Commands::Ls => {
+            let max_id = db
+                .todos
+                .iter()
+                .map(|t| t.id.to_string().len())
+                .max()
+                .unwrap_or(0);
+            let max_content = db.todos.iter().map(|t| t.content.len()).max().unwrap_or(0);
+            println!(
+                "|{:<max_id$}|{:<4}  |{:<max_content$}|",
+                "ID",
+                "Done",
+                "Content",
+                max_id = max_id + 1,
+                max_content = max_content
+            );
+            let stdout = io::stdout();
+            let mut out = stdout.lock();
+            for todo in db.todos.iter() {
+                let done_str = if todo.completed_at.is_some() {
+                    format!("[x]")
+                } else {
+                    "[ ]".to_string()
+                };
+                writeln!(
+                    out,
+                    "|{:<max_id$}|{:<4}  |{:<max_content$}|",
+                    todo.id,
+                    done_str,
+                    todo.content,
+                    max_id = max_id + 1,
+                    max_content = max_content
+                )
+                .unwrap();
+            }
         }
-        Commands::Remove { id } => {
+        Commands::Rm { id } => {
             db.todos.retain(|todo| todo.id != id);
             save_db(db);
+        }
+        Commands::Done { id } => {
+            if let Some(todo) = db.todos.iter_mut().find(|todo| todo.id == id) {
+                let now = OffsetDateTime::now_utc();
+                todo.completed_at = Some(now);
+                todo.updated_at = now;
+                save_db(db);
+            };
+        }
+        Commands::Edit { id, content } => {
+            if let Some(todo) = db.todos.iter_mut().find(|todo| todo.id == id) {
+                todo.content = content.join(" ");
+                todo.updated_at = OffsetDateTime::now_utc();
+                save_db(db);
+            };
         }
     }
 }
